8c60366c38f95e11a6bc26afb403053f9b446630
Brolen Gumb <Brolengumb@u.boisestate.edu>
2026-01-20 16:20:43 -0700

    update

--- MagicSquare.java
+++ MagicSquare.java
@@ -20,12 +20,50 @@ public class MagicSquare implements MagicSquareInterface {
 
     // Constructor takes filename & dimension integer
     public MagicSquare(String filename, int dimension) throws IOException {
+        File file = new File(filename);
+        PrintWriter outFile = new PrintWriter(new FileWriter(file));
 
+        int n =  dimension * dimension;
+        int[][] matrix = new Array[dimension][dimension];
+        int row = n - 1;
+        int col = n / 2;
+        int oldRow = row;
+        int oldCol = col;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j]
+
+            }
+        }
+
+        if(int[][] != 0) {
+            row = oldRow;
+            col = oldCol;
+            row--;
+        }
+
+        outFile.close();
     }
 
-    // Evaluates matrix & returns a boolean verdict of whether the matrix is a magic square.
+    /* 
+     * Evaluates matrix & returns a boolean verdict
+     * of whether the matrix is a magic square.
+     * A magic square is valid if the matrix consists of incrementing
+     * integrals starting at '1' & ending at the product of the
+     * given integer^2 without repeating any integers.
+     * Expl:
+     * n = 3, 3^2 = 9
+     * 4 9 2
+     * 3 5 7
+     * 8 1 6
+     */
     public boolean isMagicSquare() {
-
+        if () {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Returns a copy of the matrix as a 2D array of integers
