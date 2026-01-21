0a85b8ad8e012dcba429a10cbfcf55d6b997e1fb
Brolen Gumb <Brolengumb@u.boisestate.edu>
2026-01-20 17:33:07 -0700

    Heading home.

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,30 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * 
  * @author Brolen Gumb
  * @version 1.0
  * 
-*/
-
+ */
 
 public class MagicSquare implements MagicSquareInterface {
 
-    public MagicSquare 
+    Random rand = new Random();
+    private int[][] matrix;
 
-    Scanner scnr = new Scanner();
-    Scanner lineScnr = new Scanner();
-    
-    // Constructor takes filename & attempts to read that file
+    public MagicSquare(String filename) throws FileNotFoundException {
+        // Constructor takes filename & attempts to read that file
+        readMatrix(filename);
+
+        File file = new File(filename);
+        Scanner scnr = new Scanner(file);
+        while (scnr.hasNextLine()) {
+            String line = scnr.nextLine();
+            System.out.println(line);
+        }
+        scnr.close();
+    }
+
     private int[][] readMatrix(String filename) throws FileNotFoundException {
+        // Reads matrix from file & returns as 2D array of integers
+        File file = new File(filename);
+        Scanner scnr = new Scanner(file);
+        int dimension = scnr.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = scnr.nextInt();
+            }
+        }
+        scnr.close();
+        return matrix;
 
     }
 
     // Constructor takes filename & dimension integer
     public MagicSquare(String filename, int dimension) throws IOException {
-        File file = new File(filename);
-        PrintWriter outFile = new PrintWriter(new FileWriter(file));
 
-        int n =  dimension * dimension;
-        int[][] matrix = new Array[dimension][dimension];
+        writeMatrix(getMatrix(), filename);
+
+        int n = dimension * dimension;
+        int[][] matrix = new int[dimension][dimension];
         int row = n - 1;
         int col = n / 2;
         int oldRow = row;
@@ -32,21 +62,31 @@ public class MagicSquare implements MagicSquareInterface {
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                matrix[i][j]
+                matrix[i][j] = 0;
 
             }
         }
 
-        if(int[][] != 0) {
+        if (matrix[row % dimension][col % dimension] != 0) {
             row = oldRow;
             col = oldCol;
             row--;
         }
+    }
 
+    private void writeMatrix(int[][] matrix, String filename) throws IOException {
+        FileWriter writer = new FileWriter(filename);
+        PrintWriter outFile = new PrintWriter(writer);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                outFile.print(matrix[i][j] + " ");
+            }
+            outFile.println();
+        }
         outFile.close();
     }
 
-    /* 
+    /*
      * Evaluates matrix & returns a boolean verdict
      * of whether the matrix is a magic square.
      * A magic square is valid if the matrix consists of incrementing
@@ -58,8 +98,9 @@ public class MagicSquare implements MagicSquareInterface {
      * 3 5 7
      * 8 1 6
      */
+
     public boolean isMagicSquare() {
-        if () {
+        if (getMatrix() != null) {
             return true;
         } else {
             return false;
@@ -68,32 +109,40 @@ public class MagicSquare implements MagicSquareInterface {
 
     // Returns a copy of the matrix as a 2D array of integers
     public int[][] getMatrix() {
-
+        int[][] newMatrix = new int[matrix.length][matrix.length];
+        return newMatrix;
     }
 
     /**
-     *  Returns a string formatted as in these examples:
-     *   The matrix
-	 *     4 9 2
-	 *     3 5 7
-	 *     8 1 6
-	 *   is a magic square.
-	 *  or
-	 *    The matrix
-	 *      5 3 7
-	 *      9 4 2
-	 *      1 6 5
-	 *    is not a magic square.
+     * Returns a string formatted as in these examples:
+     * The matrix
+     * 4 9 2
+     * 3 5 7
+     * 8 1 6
+     * is a magic square.
+     * or
+     * The matrix
+     * 5 3 7
+     * 9 4 2
+     * 1 6 5
+     * is not a magic square.
      */
 
     public String toString() {
-        System.out.println("The matrix");
-        System.out.println();
-
-        if () {
-            System.out.print("is a magic square");
-        } else {
-            System.out.print("is not a magic square");
+        String result = "The matrix\n";
+        int[][] matrix = getMatrix();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result += matrix[i][j] + " ";
+            }
+            result += "\n";
         }
+
+        if (isMagicSquare()) {
+            result += "is a magic square.";
+        } else {
+            result += "is not a magic square.";
+        }
+        return result;
     }
 }
--- MagicSquareDriver.java
+++ MagicSquareDriver.java
@@ -5,11 +5,22 @@
  * 
  * @author Brolen Gumb
  * @version 1.0
-*/
+ */
 
 public class MagicSquareDriver {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        try {
+            MagicSquare magicSquareFromFile = new MagicSquare("magic3.txt");
+            System.out.println("Magic Square read from file:");
+            System.out.println(magicSquareFromFile);
+
+            int dimension = 5; // Example dimension
+            MagicSquare generatedMagicSquare = new MagicSquare("generatedMagic5.txt", dimension);
+            System.out.println("Generated Magic Square of dimension " + dimension + ":");
+            System.out.println(generatedMagicSquare);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
